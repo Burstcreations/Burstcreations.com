412e3e3c4dc1e1e5c310a7128424713fd0c0ae20
Ilya Radchenko <ilya@burstcreations.com>
2013-05-04 10:40:32 -0400

    Initial commit, static controller/message working for viewing and creating messages.

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,22 @@
+class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @messages }
+    end
+  end
+
+  def new
+    @message = Message.new(params[:message])
+
+    if @message.save
+      flash[:notice] = "Message sent successfully! Thank you!"
+    else
+      flash[:error] = "Unable to send your message. Please let me know of the issue via Twitter."
+    end
+
+    redirect_to :back
+  end
+end
