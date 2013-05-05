b2ea207603fc1a326678347567edf54a7164cf1e
knownasilya <ilya@burstcreations.com>
2013-05-05 15:22:06 -0400

    Pre beta, removed ember and fix scrollspy from Bootstrap.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require handlebars
-//= require ember
-//= require ember-data
 //= require_self
-//= require burstcreations_2013
-App = Ember.Application.create();
 
 //= require_tree .
