5f096ac435d86452ee6e3becf7e06223a9e8c3d5
Ilya Radchenko <ilya@burstcreations.com>
2014-11-21 09:34:31 -0500

    Add ghost

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express = require('express');
+var ghost = require('ghost');
 var chalk = require('chalk');
 var app = express();
 var port = process.env.PORT || 3000;
@@ -8,6 +9,21 @@ var env = process.env.NODE_ENV || 'development';
 
 app.use('/', express.static('./static'));
 
-app.listen(port, function () {
+ghost({
+  config: {
+    production: {
+      url: 'http://burstcreations.com/blog',
+      server: {
+        port: port
+      },
+      paths: {
+        subdir: '/blog'
+      }
+    }
+  }
+}).then(function (ghostServer) {
+  app.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+
+  ghostServer.start(app);
   console.log(chalk.green('Running in ' + env + ' on localhost:' + port + '..'));
 });
